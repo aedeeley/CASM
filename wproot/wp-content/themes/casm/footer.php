8939f2d1ddcfd27a2b27af56de4b70d2241c961b
Alex Deeley <aedeeley@gmail.com>
2015-08-14 12:27:36 -0400

    Responsive edits

--- wproot/wp-content/themes/casm/footer.php
+++ wproot/wp-content/themes/casm/footer.php
@@ -10,54 +10,60 @@
  */
 ?>
 
-      
-<?php if(is_home()) { ?>  
-    <div class="row affiliates">
-          <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-waketech.jpg" alt="Affiliate" /></a>
-          <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-ncworks.jpg" alt="Affiliate" /></a>
-          <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-isg.jpg" alt="Affiliate" /></a>
-          <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-capitalarea.jpg" alt="Affiliate" /></a>
-          
-          <div class="clear"></div>
-      </div>
-      </div>
-    <?php }; ?>
-      
-      
- <section id="footer">    
-    <div class="row">
-        <div class="large-4 columns">
-            <ul class="social">
-                <li><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon-fb.jpg" alt="Facebook" /></a></li>
-                <li><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon-twitter.jpg" alt="Twitter" /></a></li>
-                <li><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon-google.jpg" alt="Google Plus" /></a></li>
-            </ul>
-            
-            <a href="mailto:info@emailaddress.com">info@emailaddress.com</a><br />
-            (555) 555-5555<br /><Br />
-            <span class="copy">&copy; Copyright 2015 CASM</span>
-        </div>
-        <div class="large-4 columns"><span class="yellow">@twitterfeed</span></div>
-        <div class="large-4 columns">
-            <div id="newsletter">
-                <span class="yellow">Ready to enroll?</span><br ?>
-                
-                <input type="text" id="subject" placeholder="You can add a subject" /><br />
-                Message<br />
-                <textarea id="message" placeholder="Leave a message" rows="7"></textarea><br />
-                <a href="#" class="button hvr-sweep-to-top">Send mail</a>                
-            </div>
-        </div>
-    </div>
-    
-    
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
 
-<?php wp_footer(); ?>
+    <?php if(is_home()) { ?>
+        <div class="row affiliates">
+            <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-waketech.jpg" alt="Affiliate" /></a>
+            <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-ncworks.jpg" alt="Affiliate" /></a>
+            <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-isg.jpg" alt="Affiliate" /></a>
+            <a href="#" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/affil-capitalarea.jpg" alt="Affiliate" /></a>
 
-</body>
-</html>
+            <div class="clear"></div>
+        </div>
+        </div>
+        <?php }; ?>
+
+
+            <section id="footer">
+                <div class="row">
+                    <div class="large-4 columns">
+                        <ul class="social">
+                            <li><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon-fb.jpg" alt="Facebook" /></a></li>
+                            <li><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon-twitter.jpg" alt="Twitter" /></a></li>
+                            <li><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icon-google.jpg" alt="Google Plus" /></a></li>
+                        </ul>
+
+                        <a href="mailto:info@emailaddress.com">info@emailaddress.com</a>
+                        <br /> (555) 555-5555
+                        <br />
+                        <Br />
+                        <span class="copy">&copy; Copyright 2015 CASM</span>
+                    </div>
+                    <div class="large-4 columns"><span class="yellow">@twitterfeed</span></div>
+                    <div class="large-4 columns">
+                        <div id="newsletter">
+                            <span class="yellow">Ready to enroll?</span>
+                            <br ?>
+
+                            <input type="text" id="subject" placeholder="You can add a subject" />
+                            <br /> Message
+                            <br />
+                            <textarea id="message" placeholder="Leave a message" rows="7"></textarea>
+                            <br />
+                            <a href="#" class="button hvr-sweep-to-top">Send mail</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <script src="js/vendor/jquery.js"></script>
+                <script src="js/foundation.min.js"></script>
+                <script>
+                    $(document).foundation();
+                </script>
+
+                <?php wp_footer(); ?>
+
+                    </body>
+
+                    </html>
--- wproot/wp-content/themes/casm/header.php
+++ wproot/wp-content/themes/casm/header.php
@@ -8,45 +8,45 @@
  * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
  */
-?><!doctype html>
-<html class="no-js" lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php wp_head(); ?>
-    <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/css/foundation.css" />
-    <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/css/hover-min.css" />
-    <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/css/custom.css" />
-    <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr.js"></script>
-</head>
+?>
+    <!doctype html>
+    <html class="no-js" lang="en">
 
-<body <?php body_class(); ?>>
-<a href="#" class="mobile"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="CASM logo" /></a>
-<section id="nav" role="navigation">
-    <div class="row">
-        <label for="show-menu" class="show-menu">Show Menu</label>
-        <input type="checkbox" id="show-menu" role="button">
-        <ul id="menu">
-            <li><a href="#" class="hvr-underline-reveal">What is Cybersecurity?</a></li>  
-            <li><a href="#" class="hvr-underline-reveal">Program Overview</a></li>
-            <li id="logo"><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.png" alt="CASM Logo" /></a></li>  
-            <li><a href="#" class="hvr-underline-reveal">Why Partner?</a></li>  
-            <li><a href="#" class="hvr-underline-reveal">Industry</a></li>  
-            <li><a href="#" class="hvr-underline-reveal">Contact</a></li>    
-        </ul>
-      </div>
-</section>  
-      
-<?php if(is_home()) { ?>
-<section id="slider">    
-    <div class="row">
-      <div class="large-12 columns">
-        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/slide.jpg" alt="Slider" />
-      </div>
-    </div>
-</section>  
-    
-    <?php }; ?>
-      
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <?php wp_head(); ?>
+            <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/css/foundation.css" />
+            <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/css/hover-min.css" />
+            <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/css/custom.css" />
+            <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr.js"></script>
+    </head>
+
+    <body <?php body_class(); ?>>
+        <a href="#" class="mobile"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="CASM logo" /></a>
+        <section id="nav" role="navigation">
+            <div class="row">
+                <label for="show-menu" class="show-menu">Show Menu</label>
+                <input type="checkbox" id="show-menu" role="button">
+                <ul id="menu">
+                    <li><a href="#" class="hvr-underline-reveal">What is Cybersecurity?</a></li>
+                    <li><a href="#" class="hvr-underline-reveal">Program Overview</a></li>
+                    <li id="logo"><a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.png" alt="CASM Logo" /></a></li>
+                    <li><a href="#" class="hvr-underline-reveal">Why Partner?</a></li>
+                    <li><a href="#" class="hvr-underline-reveal">Industry</a></li>
+                    <li><a href="#" class="hvr-underline-reveal">Contact</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <?php if(is_home()) { ?>
+            <section id="slider">
+                <div class="row">
+                    <div class="large-12 columns">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/slide.jpg" alt="Slider" />
+                    </div>
+                </div>
+            </section>
+            <?php }; ?>
